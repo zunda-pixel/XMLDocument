574c8efdab8d707b9ba6da4e82147fac9ee981b6
zunda.dev@gmail.com <zunda.dev@gmail.com>
2024-11-25 20:32:19 +0900

    add interoperabilityMode

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.10
 //
 //  Copyright (c) 2019 Changbeom Ahn
 //
@@ -25,31 +25,37 @@ import PackageDescription
 
 let package = Package(
     name: "JebiXML",
+    platforms: [
+        .iOS(.v16),
+        .macOS(.v13)
+    ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "JebiXML",
-            targets: ["JebiXML"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+            targets: ["JebiXML"]
+        ),
     ],
     targets: [
         .target(
             name: "JebiXML",
             dependencies: ["libxml2XMLDocument"],
-            path: "Sources/XMLDocument"),
+            path: "Sources/XMLDocument",
+            swiftSettings: [
+                .interoperabilityMode(.Cxx),
+            ]
+        ),
         .testTarget(
             name: "XMLDocumentTests",
-            dependencies: ["JebiXML"]),
+            dependencies: ["JebiXML"]
+        ),
         .systemLibrary(
             name: "libxml2XMLDocument",
             path: "Sources/libxml2",
             pkgConfig: "libxml-2.0",
             providers: [
                 .brew(["libxml2"]),
-//                .apt(["libxml2-dev"])
-            ]),
+                //        .apt(["libxml2-dev"])
+            ]
+        ),
     ]
 )
